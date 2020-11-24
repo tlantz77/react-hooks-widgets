2ef80457af0e093eb9756434a14b4fd1613a0e66
Tim Lantz <tlantz@gmail.com>
2020-11-24 12:15:43 -0600

    fix small bug in search widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('otter');
+  const [term, setTerm] = useState('');
   const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
@@ -17,21 +17,22 @@ const Search = () => {
   }, [term]);
 
   useEffect(() => {
-    const search = async () => {
-      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-        params: {
-          action: 'query',
-          list: 'search',
-          origin: '*',
-          format: 'json',
-          srsearch: debouncedTerm,
-        },
-      });
-
-      setResults(data.query.search);
-    };
-
-    search();
+    if (debouncedTerm) {
+      const search = async () => {
+        const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+          params: {
+            action: 'query',
+            list: 'search',
+            origin: '*',
+            format: 'json',
+            srsearch: debouncedTerm,
+          },
+        });
+        setResults(data.query.search);
+      };
+  
+      search();
+    }
   }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
